b6a57ca757385a36e6eeb29232eede3d3f85f784
eboboworld <vitya201113@gmai.com>
2021-02-24 19:10:03 +0200

    add-tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component  { 
+  state = {
+    inputValue: "",
+    tasks: []
+  }
 
-export default App;
+  inputChange = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  }
+
+  buttonSubmit = (e) => {
+    this.setState({
+      tasks: [this.state.inputValue, ...this.state.tasks],
+      inputValue:"",
+    });
+  };
+
+ 
+  render(){ 
+
+    return (
+      <div>
+        <h1>ToDo Task</h1>
+        <input placeholder="Enter task..." value={this.state.inputValue} onChange={this.inputChange} />
+        <button onClick={this.buttonSubmit}>Add</button>
+        <ul>
+          {this.state.tasks.map((tasks, index) => (
+            <li key={index}>{tasks}</li>
+            ))}
+        </ul>
+      </div>
+    )
+  }
+
+
+} 
+  
+export default App; 
